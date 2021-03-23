4332732a84fce2993b62be85eebf6da69063f981
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-23 09:11:57 +0200

    Maximum and Minimum Element

---
+++ Advanced/Advanced/Lists as Stacks and Queues - Exercise/2.Maximum and Minimum Element.py
@@ -0,0 +1,46 @@
+stack = []
+
+numbers_of_queries = int(input())
+
+for _ in range(numbers_of_queries):
+
+    data = input().split()
+
+    type_of_query = data[0]
+
+    if type_of_query == '1':
+        element = int(data[1])
+        stack.append(element)
+    elif type_of_query == '2':
+        if stack:
+            stack.pop()
+    elif type_of_query == '3':
+        if stack:
+            temp_stack = []
+            max_num = 0
+            while stack:
+                num = stack.pop()
+                temp_stack.append(num)
+                if num > max_num:
+                    max_num = num
+            print(max_num)
+            while temp_stack:
+                stack.append(temp_stack.pop())
+    elif type_of_query == '4':
+        if stack:
+            temp_stack = []
+            min_num = 1000
+            while stack:
+                num = stack.pop()
+                temp_stack.append(num)
+                if num < min_num:
+                    min_num = num
+            print(min_num)
+            while temp_stack:
+                stack.append(temp_stack.pop())
+
+output_stack = []
+
+while stack:
+    output_stack.append(stack.pop())
+print(*output_stack, sep=', ')
