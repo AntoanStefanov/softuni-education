b27f86d7982d0da597e0bf6e51c0e6e7cc7e765b
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-23 10:05:34 +0200

    Robotics
    In process

---
+++ Advanced/Advanced/Lists as Stacks and Queues - Exercise/7.Robotics.py
@@ -0,0 +1,42 @@
+from collections import deque
+from time import strftime, gmtime
+
+
+def get_sec(time_str):
+    """Get Seconds from time."""
+    h, m, s = time_str.split(':')
+    return int(h) * 3600 + int(m) * 60 + int(s)
+
+
+robots = {}
+
+robots_info = input().split(';')
+for r in robots_info:
+    robot, time_processing = r.split('-')
+    time_processing = int(time_processing)
+    is_availabe = True
+    available_at = ''
+    robots[robot] = [time_processing, is_availabe, available_at]
+
+start_time = input()
+total_seconds = get_sec(start_time)
+
+products = deque()
+while True:
+    product = input()
+    if product == 'End':
+        break
+    products.append(product)
+
+
+while products:
+    total_seconds += 1
+    for robot, info in robots.items():
+        if info[1]:
+            print(
+                f"{robot} - {products.popleft()} [{strftime('%H:%M:%S', gmtime(total_seconds))}]")
+            info[2] = strftime('%H:%M:%S', gmtime(total_seconds + info[0]))
+            info[1] = False
+            # TODO
+        else:
+            pass  # TODO
