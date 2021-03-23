5dcce845cbeb890e14bdfec27db2ff8be8d2046f
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-23 09:13:31 +0200

    Fast Food

---
+++ Advanced/Advanced/Lists as Stacks and Queues - Exercise/3. Fast Food.py
@@ -0,0 +1,58 @@
+# from collections import deque
+# total_food = int(input())
+
+# orders = deque([int(x) for x in input().split()])  # robots_statistics
+
+# # biggest we can use max() also it is for list and robots_statistics
+# order = 1
+# biggest_order = 0
+# while order <= len(orders):
+#     current_order = orders.popleft()
+#     if current_order > biggest_order:
+#         biggest_order = current_order
+#     orders.append(current_order)
+#     order += 1
+# print(biggest_order)
+
+
+# while orders:
+#     order = orders.popleft()
+#     if total_food >= order:
+#         total_food -= order
+#     else:
+#         print('Orders left:', end=' ')
+#         orders.appendleft(order)
+#         print(orders.popleft(), end=' ')
+#         break
+
+# else:
+#     print('Orders complete')
+
+
+# WITH robots_statistics INDEX ACCESSING IS IT POSSIBLE = peek deque[0]  ??
+from collections import deque
+total_food = int(input())
+
+orders = deque([int(x) for x in input().split()])  # robots_statistics
+
+
+order = 1
+biggest_order = 0
+while order <= len(orders):
+    current_order = orders.popleft()
+    if current_order > biggest_order:
+        biggest_order = current_order
+    orders.append(current_order)
+    order += 1
+print(biggest_order)
+
+
+while orders:
+    if total_food < orders[0]:
+        orders_left = [str(order) for order in orders]
+        print(f"Orders left: {' '.join(orders_left)}")
+        break
+    ordered_food = orders.popleft()
+    total_food -= ordered_food
+else:
+    print('Orders complete')
