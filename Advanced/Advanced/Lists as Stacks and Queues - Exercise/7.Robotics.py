9bc13c0acca092a4afb22c3f9434acf0de4fa9b8
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-24 10:53:17 +0200

    Robotics

--- Advanced/Advanced/Lists as Stacks and Queues - Exercise/7.Robotics.py
+++ Advanced/Advanced/Lists as Stacks and Queues - Exercise/7.Robotics.py
@@ -1,45 +1,59 @@
 from collections import deque
-from time import strftime, gmtime
 
 
-def get_sec(time_str):
-    """Get Seconds from time."""
-    h, m, s = time_str.split(':')
-    return int(h) * 3600 + int(m) * 60 + int(s)
+def next_second(h, m, s):
+    s += 1
+    if s == 60:
+        m += 1
+        s = 0
+    if m == 60:
+        h += 1
+        m = 0
+    if h == 24:
+        h = 0
 
+    return h, m, s
 
-available_robots = deque()
-busy_robots = deque()
 
 robots_info = input().split(';')
+robot_dict = {}
+available_robots = deque()
+waiting_robots = deque()
+products = deque()
+
+time = [int(x) for x in input().split(':')]
+
 for r in robots_info:
     robot, time_processing = r.split('-')
     time_processing = int(time_processing)
-    available_robots.append([robot, time_processing, True, ''])
+    available_robots.append([robot, time_processing])
+    robot_dict[robot] = time_processing
 
 
-start_time = input()
-total_seconds = get_sec(start_time)
-
-products = deque()
 while True:
     product = input()
     if product == 'End':
         break
     products.append(product)
 
-
 while products:
-    total_seconds += 1
-    product = products.popleft()
-    if available_robots:
-        available_robot = available_robots.popleft()
-        available_robot[2] = False
-        print(
-            f"{available_robot[0]} - {product} [{strftime('%H:%M:%S', gmtime(total_seconds))}]")
 
-        available_robot[3] = strftime(
-            '%H:%M:%S', gmtime(total_seconds + available_robot[1]))
-        busy_robots.append(available_robot)
-    else:
-       # What if there are no available robots ?
+    for robot in waiting_robots:
+        robot_name = robot[0]
+        robot[1] -= 1
+        if robot[1] <= 0:  # ако е бил 0 и го намаля с 1 пак тря се върне в свободни роботи
+            available_robots.append([robot_name, robot_dict[robot_name]])
+    # Понеже през фор , не може да ме маха ще направи филтрация за освобоените роботи
+    waiting_robots = [r for r in waiting_robots if r[1] > 0]
+
+    time = next_second(time[0], time[1], time[2])
+    product = products.popleft()
+
+    if not available_robots:
+        products.append(product)
+        continue
+
+    current_robot = available_robots.popleft()
+    print(
+        f"{current_robot[0]} - {product} [{time[0]:02d}:{time[1]:02d}:{time[2]:02d}]")
+    waiting_robots.append(current_robot)
