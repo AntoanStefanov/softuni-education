c62f582068ce57d0b5b87ed160cb4183bbffb5a1
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-26 08:58:57 +0200

    Count Same Values
    Solution with imported defaultdict.

--- Advanced/Advanced/Tuples and Sets - Lab/1.Count Same Values.py
+++ Advanced/Advanced/Tuples and Sets - Lab/1.Count Same Values.py
@@ -1,3 +1,20 @@
+from collections import defaultdict
+
+
+numbers = input().split()
+# If the key doesn't exist in the dict, factory will construct the key and value 0'
+occurrences = defaultdict(int)  # default factory  int , which will return 0 !
+
+for num in numbers:
+    num = float(num)
+    occurrences[num] += 1
+
+for num, occur in occurrences.items():
+    print(f"{num} - {occur} times")
+
+
+# Second solution
+
 numbers = input().split()
 
 occurrences = {}
