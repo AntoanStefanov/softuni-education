6976c55754b9f081a226cc4d223f0a781b7db6e6
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-26 11:06:42 +0200

    Average Student Grades

--- Advanced/Advanced/Tuples and Sets - Lab/2.Average Student Grades.py
+++ Advanced/Advanced/Tuples and Sets - Lab/2.Average Student Grades.py
@@ -8,6 +8,7 @@ for _ in range(number_of_students):
     grade = float(grade)
     student_records[name].append(grade)
 
-print(student_records)
-for info in student_records.items():
-    print(info)
+for name, grades in student_records.items():
+    average_grade = sum(grades) / len(grades)
+    grades = [f'{grade:.2f}' for grade in grades]
+    print(f"{name} -> {' '.join(grades)} (avg: {average_grade:.2f})")
