14c7edb33008a53756e62eb1b80261d9b2d14ca8
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-26 19:34:27 +0200

    Parking Lot

--- Advanced/Advanced/Tuples and Sets - Lab/4.Parking Lot.py
+++ Advanced/Advanced/Tuples and Sets - Lab/4.Parking Lot.py
@@ -4,13 +4,21 @@ parking_lot = set()
 
 for _ in range(number_of_cars):
     direction, car_number = input().split(', ')
-    if direction == 'IN':
-        parking_lot.add(car_number)
-    else:
-        parking_lot.remove(car_number)
+    # Dictionary
+    directions = {
+        'IN': parking_lot.add,
+        'OUT': parking_lot.remove,
+
+    }
+    directions[direction](car_number)
+     ### OR ### 
+     
+    # if direction == 'IN':
+    #     parking_lot.add(car_number)
+    # else:
+    #     parking_lot.remove(car_number)
 
 if parking_lot:
-    for car_number in parking_lot:
-        print(car_number)
+    print('\n'.join(parking_lot))
 else:
     print('Parking Lot is Empty')
