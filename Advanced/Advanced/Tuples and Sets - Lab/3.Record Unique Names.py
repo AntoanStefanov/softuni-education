c355f81ff8da7a5880a6bbe8b9480de8f5087abc
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-26 13:21:28 +0200

    Record Unique Names

--- Advanced/Advanced/Tuples and Sets - Lab/3.Record Unique Names.py
+++ Advanced/Advanced/Tuples and Sets - Lab/3.Record Unique Names.py
@@ -1,3 +1,19 @@
+# Adding the name in the set directly , if the name already exists,
+# it doesn't add second of the same name. 
+
+number_of_names = int(input())
+names = set()
+for _ in range(number_of_names):
+    name = input()
+    names.add(name)
+
+for name in names:
+    print(name)
+
+
+############ List and Set ##
+
+
 number_of_names = int(input())
 names = []
 for _ in range(number_of_names):
@@ -6,4 +22,3 @@ for _ in range(number_of_names):
 set_names = set(names)
 for name in set_names:
     print(name)
-
