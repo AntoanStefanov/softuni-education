5eca80d6ddade5bbb09b141141eb1e7991a481e3
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-27 17:40:24 +0200

    Longest Intersection

---
+++ Advanced/Advanced/Tuples and Sets - Exercise/6.Longest Intersection.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+longest_intersection = set()
+
+for _ in range(n):
+    first_range, second_range = input().split('-')
+    first_range = first_range.split(',')
+    first_start = int(first_range[0])
+    first_end = int(first_range[1])
+    second_range = second_range.split(',')
+    second_start = int(second_range[0])
+    second_end = int(second_range[1])
+
+    first_set = set()
+    second_set = set()
+
+    for num in range(first_start, first_end + 1):
+        first_set.add(num)
+
+    for num in range(second_start, second_end + 1):
+        second_set.add(num)
+
+    current_intersection = first_set.intersection(second_set)
+
+    if len(current_intersection) > len(longest_intersection):
+        longest_intersection = current_intersection
+
+print(
+    f'Longest intersection is {list(longest_intersection)} with length {len(longest_intersection)}')
