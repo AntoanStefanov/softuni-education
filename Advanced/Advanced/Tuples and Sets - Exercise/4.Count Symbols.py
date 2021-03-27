a21e39c1d062316fd50546a93c37894cc3e73a7d
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-27 13:36:56 +0200

    Count Symbols

--- Advanced/Advanced/Tuples and Sets - Exercise/4.Count Symbols.py
+++ Advanced/Advanced/Tuples and Sets - Exercise/4.Count Symbols.py
@@ -23,3 +23,20 @@ for char in string:
 
 for key, value in sorted(symbol_occurrences.items()):
     print(f"{key}: {value} time/s")
+
+########### OR #################
+
+string = input()
+
+symbol_occurrences = {}
+
+for char in string:
+    if char not in symbol_occurrences:
+        symbol_occurrences[char] = 0
+
+    symbol_occurrences[char] += 1
+
+sorted_dict = sorted(symbol_occurrences)
+
+for key in sorted_dict:
+    print(f"{key}: {symbol_occurrences[key]} time/s")
