a5f237565e0528c3b92efb88680be3704aa3958c
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-27 06:46:50 +0200

    Parking Lot

--- Advanced/Advanced/Tuples and Sets - Lab/4.Parking Lot.py
+++ Advanced/Advanced/Tuples and Sets - Lab/4.Parking Lot.py
@@ -7,9 +7,12 @@ for _ in range(number_of_cars):
     # Dictionary
     directions = {
         'IN': parking_lot.add,
-        # remove method will return keyerror if the car does not exist in the set.
-        # discard method will ignore if we remove a car that doesn't exist in the set
-        # yet judge gives 100/100 with remove.
+        # Remove method will return keyerror if the car does not exist in the set.
+        # Discard method will ignore if we remove a car that doesn't exist in the set
+        # Yet judge gives 100/100 with remove
+        # We can use if statement to check if the car exists in the set
+        # And then remove method , if it doesn't exist in the set, Do nothing
+        # Which is equal to discard method.
         'OUT': parking_lot.discard,
 
     }
