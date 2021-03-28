89f0a029091196b07ffd0808aafea31bbbda5aef
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-28 11:34:42 +0300

    Sum Matrix Elements

--- Advanced/Advanced/Multidimensional Lists - Lab/01. Sum Matrix Elements.py
+++ Advanced/Advanced/Multidimensional Lists - Lab/01. Sum Matrix Elements.py
@@ -1,13 +1,24 @@
+# rows, columns = [int(x) for x in input().split(', ')]
+
+# matrix = []
+# matrix_elements_sum = 0
+
+# for r in range(rows):
+#     row_elements = [int(x) for x in input().split(', ')]
+#     matrix.append(row_elements)
+
+#     matrix_elements_sum += sum(row_elements)
+
+# print(matrix_elements_sum)
+# print(matrix)
+
+
+##### Comprehensions ###################
+import itertools
 rows, columns = [int(x) for x in input().split(', ')]
 
-matrix = []
-matrix_elements_sum = 0
-
-for r in range(rows):
-    row_elements = [int(x) for x in input().split(', ')]
-    matrix.append(row_elements)
-
-    matrix_elements_sum += sum(row_elements)
-    
-print(matrix_elements_sum)
+matrix = [[int(x) for x in input().split(', ')] for r in range(rows)]
+# Подаване като позиционен аргумент всеки един от редовете в матрицата *
+matrix_sum = sum(itertools.chain(*matrix))
+print(matrix_sum)
 print(matrix)
