71734e25355f1e4075b59890fc44b4c36086fb29
AntoanStefanov <antoine.stefanov@gmail.com>
2021-03-28 16:42:22 +0300

    Sum Matrix Columns

---
+++ Advanced/Advanced/Multidimensional Lists - Lab/02. Sum Matrix Columns.py
@@ -0,0 +1,26 @@
+rows, cols = [int(x) for x in input().split(', ')]
+
+matrix = []
+
+for r in range(rows):
+    elements = [int(x) for x in input().split(' ')]
+    matrix.append(elements)
+
+for col in range(cols):
+    column_sum = 0
+    for r in range(rows):
+        column_sum += matrix[r][col]
+    print(column_sum)
+
+
+################################################################
+# main loop is for columns, child loop for the rows
+# matrix[0][0]
+# matrix[1][0]
+# matrix[2][0]
+
+# matrix[0][1]
+# matrix[1][1]
+# matrix[2][1]
+
+# ............
