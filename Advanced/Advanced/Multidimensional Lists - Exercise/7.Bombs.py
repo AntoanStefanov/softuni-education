0e0a8703226956aa8a19c4a83da855bb970b4ab1
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-01 21:44:49 +0300

    Bombs

--- Advanced/Advanced/Multidimensional Lists - Exercise/7.Bombs.py
+++ Advanced/Advanced/Multidimensional Lists - Exercise/7.Bombs.py
@@ -8,7 +8,6 @@ for r in range(rows):
     matrix.append(row)
 
 
-
 bombs_info = input().split()
 
 bombs_coordinates = []
@@ -23,19 +22,49 @@ for bomb_coordinates in bombs_coordinates:
     bomb_power = matrix[row][column]
 
     if (row - 1 >= 0) and (column-1 >= 0):
-        matrix[row-1][column-1] -= bomb_power
+
+        if matrix[row-1][column-1] > 0:
+            matrix[row-1][column-1] -= bomb_power
     if (row - 1 >= 0):
-        matrix[row-1][column] -= bomb_power
+
+        if matrix[row-1][column] > 0:
+            matrix[row-1][column] -= bomb_power
     if (row - 1 >= 0) and (column+1 < columns):
-        matrix[row-1][column+1] -= bomb_power
+
+        if matrix[row-1][column+1] > 0:
+            matrix[row-1][column+1] -= bomb_power
     if column + 1 < columns:
-        matrix[row][column+1] -= bomb_power
+
+        if matrix[row][column+1] > 0:
+            matrix[row][column+1] -= bomb_power
     if (row + 1) < rows and (column+1 < columns):
-        matrix[row+1][column+1] -= bomb_power
+
+        if matrix[row+1][column+1] > 0:
+            matrix[row+1][column+1] -= bomb_power
     if row + 1 < rows:
-        matrix[row+1][column] -= bomb_power
-    if row + 1 < rows and columns - 1 >= 0:
-        matrix[row+1][column-1] -= bomb_power
-    if columns - 1 >= 0:
-        matrix[row][column-1] -= bomb_power
+
+        if matrix[row+1][column] > 0:
+            matrix[row+1][column] -= bomb_power
+    if row + 1 < rows and column - 1 >= 0:
+
+        if matrix[row+1][column-1] > 0:
+            matrix[row+1][column-1] -= bomb_power
+    if column - 1 >= 0:
+
+        if matrix[row][column-1] > 0:
+            matrix[row][column-1] -= bomb_power
     matrix[row][column] = 0
+
+
+alive_cells = 0
+sum_cells = 0
+
+for row in matrix:
+    for el in row:
+        if el > 0:
+            alive_cells += 1
+            sum_cells += el
+
+print(f'Alive cells: {alive_cells}')
+print(f"Sum: {sum_cells}")
+
