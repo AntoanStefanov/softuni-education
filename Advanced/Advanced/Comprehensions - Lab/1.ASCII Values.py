bb221001c4fd498a064297556fb663ebb28ff008
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-04 15:24:53 +0300

    ASCII Values
    With comprehension and standard syntax

--- Advanced/Advanced/Comprehensions - Lab/1.ASCII Values.py
+++ Advanced/Advanced/Comprehensions - Lab/1.ASCII Values.py
@@ -1,3 +1,12 @@
+# Comprehension
 chars = input().split(', ')
 ascii_values = {char: ord(char) for char in chars}
 print(ascii_values)
+
+
+# Same as standard syntax
+
+my_dict = {}
+for c in chars:
+    my_dict[c] = ord(c)
+print(my_dict)
