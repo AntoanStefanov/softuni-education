ee3d9556f3d2ebf1b849196c9cd23eb9ba690e83
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-05 10:39:42 +0300

    No Vowels

--- Advanced/Advanced/Comprehensions - Lab/2.No Vowels.py
+++ Advanced/Advanced/Comprehensions - Lab/2.No Vowels.py
@@ -9,7 +9,7 @@ string = input()
 list_with_no_vowels = [
     char
     for char in string
-    if char.lower() not in vowels
+     if char.lower() not in vowels
 ]
 # The Above is equal to [char for char in string if char.lower( not in vowels)]
 print(''.join(list_with_no_vowels))
