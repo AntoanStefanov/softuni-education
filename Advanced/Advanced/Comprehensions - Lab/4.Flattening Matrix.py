5adab437d4d360011e80cd4900264de92c82c5f8
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-05 11:17:13 +0300

    Flattening Matrix

--- Advanced/Advanced/Comprehensions - Lab/4.Flattening Matrix.py
+++ Advanced/Advanced/Comprehensions - Lab/4.Flattening Matrix.py
@@ -6,7 +6,9 @@ for row in range(rows):
     elements = [int(x) for x in input().split(', ')]
     matrix.append(elements)
 
-for sublist in matrix:
-    for el in sublist:
-        flatten_matrix.append(el)
+flatten_matrix = [
+    el
+    for sublist in matrix
+    for el in sublist
+]
 print(flatten_matrix)
