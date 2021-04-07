bbf08900073fde4c0b6e403c5e58e9db5b641594
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-07 13:04:50 +0300

    Info

--- Advanced/Advanced/Functions Advanced - Lab/info.py
+++ Advanced/Advanced/Functions Advanced - Lab/info.py
@@ -1,3 +1,6 @@
+########################## LAMBDA #########
+
+
 # x = lambda a: a + 10  ||| variable = keyword argument: expression
 # print(x(5))
 # Commented beacuse pep-8 transofrms it as the below one..
@@ -16,7 +19,7 @@ def fn(a): return lambda b: a + b  # equals to fn = lambda a: lambda b: a + b
 # Function defining another function inside,
 # and the inner funtion has access to the outer function's scope(variables)
 print(fn(1))
-# that returns new lambda expression
+# We call the outer function and returns as a result the inner funtions.
 # We can call the inner function
 print(fn(1)(2))
 # Which evaluate the expression a + b
@@ -37,3 +40,8 @@ filtered_nums = list(filter(lambda x: x % 2 == 0, [1, 2, 3, 4, 5, 6]))
 mapped_nums = list(map(lambda x: x % 2 == 0, [1, 2, 3, 4, 5, 6]))  # x: x**2
 print(filtered_nums)
 print(mapped_nums)  # Тук се присвоява ретърна от lambdata
+
+
+############## Packing Arguments *args * kwargs #################################
+
+
