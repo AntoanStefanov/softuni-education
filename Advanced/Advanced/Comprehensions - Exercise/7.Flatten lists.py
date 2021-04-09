16ca9fc97b276d02d4a62f89a1bc1c3048ec7106
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-09 14:54:17 +0300

    Flatten Lists

--- Advanced/Advanced/Comprehensions - Exercise/7.Flatten lists.py
+++ Advanced/Advanced/Comprehensions - Exercise/7.Flatten lists.py
@@ -1,4 +1,5 @@
-flatten_lists_strings = [l.split() for l in input().split('|')]
-flatten_lists_strings.reverse()
-[print(el, end=' ') for l in flatten_lists_strings for el in l]
+# flatten_lists_strings = [l.split() for l in input().split('|')]
+# flatten_lists_strings.reverse()
+# [print(el, end=' ') for l in flatten_lists_strings for el in l]
 
+[print(el, end=' ') for l in input().split('|')[::-1] for el in l.split()]
