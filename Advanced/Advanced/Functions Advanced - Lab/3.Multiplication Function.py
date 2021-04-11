5cc897ddbc427b94815da906508bef53a1392fe2
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-11 20:35:13 +0300

    Multiplication Function

--- Advanced/Advanced/Functions Advanced - Lab/3.Multiplication Function.py
+++ Advanced/Advanced/Functions Advanced - Lab/3.Multiplication Function.py
@@ -1,3 +1,6 @@
+
+
+
 def multiply(*args):
     result = 1
     for arg in args:
@@ -6,3 +9,19 @@ def multiply(*args):
 
 
 print(multiply(4, 5, 6, 1, 3))
+
+################
+# reduce() - редуциране . 1-ви аргумент приема функция, която бива извиквана
+# втори аргумент - последователност , обект, който може да е обходен
+# Inital - opitnal third parameter
+
+from functools import reduce
+
+
+def multiply1(*args):
+    # първия аргумент (функцията) задължително приема два аргумента
+    return reduce(lambda a, b: a * b, args)
+
+
+# Взима два по два аргумента в ламбдата и връща резултата в последователността до изчерпване
+print(multiply(1, 2, 3, 4))
