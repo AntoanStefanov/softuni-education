57a887e41d7ba04ec1df2d5300cd362d9013d8e0
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-12 15:20:35 +0300

    Operate

--- Advanced/Advanced/Functions Advanced - Lab/4.Operate.py
+++ Advanced/Advanced/Functions Advanced - Lab/4.Operate.py
@@ -3,6 +3,8 @@ from functools import reduce
 
 def operate(operator, *args):
     if operator == '+':
+        # Inital value should be provided reduce take 3rd optional arg
+        # but judge gives 60/100 with 3rd arg (0, 0, 1, 1) so remove
         result = reduce(lambda x, y: x + y, args)
     elif operator == '-':
         result = reduce(lambda x, y: x - y, args)
