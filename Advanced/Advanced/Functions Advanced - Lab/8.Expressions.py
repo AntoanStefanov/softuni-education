e6cd50f777fb0513ec9f7a8188b2591e165b1e7b
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-13 12:00:01 +0300

    Expressions

--- Advanced/Advanced/Functions Advanced - Lab/8.Expressions.py
+++ Advanced/Advanced/Functions Advanced - Lab/8.Expressions.py
@@ -1,4 +1,4 @@
-from itertools import permutations, chain
+from itertools import permutations, chain, product
 
 
 data = input().split(', ')
@@ -9,3 +9,16 @@ for p in list(perms):
     expression = ''.join(chain(*zip(p, data)))
     res = eval(expression)
     print(f'{expression}={res}')
+
+####################
+
+
+numbers = input().split(', ')
+n = len(numbers)
+
+cartesian_product = [list(x) for x in (product('+-', repeat=n))]
+
+for signs in cartesian_product:
+    elements = [''.join(x) for x in list(zip(signs, numbers))]
+    expression = ''.join(elements)
+    print(f'{expression}={eval(expression)}')
