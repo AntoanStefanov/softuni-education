9807fc01977b2b162ccc4781ed50de140ed25571
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-15 09:26:32 +0300

    Age Assignment

---
+++ Advanced/Advanced/Functions Advanced - Exercise/11.Age Assignment.py
@@ -0,0 +1,10 @@
+def age_assignment(*args, **kwargs):
+    names_ages = {}
+    for name in args:
+        first_letter = name[0]
+        names_ages[name] = kwargs[first_letter]
+    return names_ages
+
+
+print(age_assignment("Peter", "George", G=26, P=19))
+print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
