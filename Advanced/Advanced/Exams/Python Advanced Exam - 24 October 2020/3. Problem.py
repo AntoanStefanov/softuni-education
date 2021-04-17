ed734455f16d0987f1e0f21d8c1589072939a74a
AntoanStefanov <antoine.stefanov@gmail.com>
2021-04-17 16:03:20 +0300

    3. Problem

--- Advanced/Advanced/Exams/Python Advanced Exam - 24 October 2020/3. Problem.py
+++ Advanced/Advanced/Exams/Python Advanced Exam - 24 October 2020/3. Problem.py
@@ -1,3 +1,4 @@
+ ############# WITH DEQUE rotate ############ DEQUE HAS NO SLICING !
 from collections import deque
 def best_list_pureness(numbers, k):
     numbers = deque(numbers)
@@ -16,7 +17,28 @@ def best_list_pureness(numbers, k):
     return f'Best pureness {max_pureness} after {max_pureness_rotate} rotations'
 
 
+#################### WITH LISTS AND SLICING #######
+
+# def best_list_pureness(numbers, k):
+#     max_pureness = 0
+#     max_pureness_rotate = None
+#     for rotate in range(k + 1):
+#         current_pureness = 0
+#         for index, num in enumerate(numbers):
+#             current_pureness += index * num
+#         if current_pureness > max_pureness:
+#             max_pureness = current_pureness
+#             max_pureness_rotate = rotate
+
+#         numbers = numbers[-1:] + \
+#             numbers[:-1]   # default n = 1 to right
+
+#     return f'Best pureness {max_pureness} after {max_pureness_rotate} rotations'
+
+
 test = ([7, 9, 2, 5, 3, 4], 3)
 result = best_list_pureness(*test)
 print(result)
-
+test = ([4, 3, 2, 6], 4)
+result = best_list_pureness(*test)
+print(result)
