ae51a368293db0848c293f47a7574e188f0d2df7
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-01 08:54:47 +0300

    Class Library

---
+++ Advanced/OOP/Classes and Instances - Exercise/7. Library/project/library.py
@@ -0,0 +1,30 @@
+class Library:
+    def __init__(self):
+        self.user_records = []
+        self.books_available = {}  # {author: list of books}
+        self.rented_books = {}  # {usernames: {book names: days left}}
+
+    def add_user(self, user):
+        if user in self.user_records:
+            return f"User with id = {user.user_id} already registered in the library!"
+        self.user_records.append(user)
+
+    def remove_user(self, user):
+        if not user in self.user_records:
+            return "We could not find such user to remove!"
+        self.user_records.remove(user)
+        if user in self.rented_books:
+            # Do the books go back in books_available ?
+            self.rented_books.pop(user.username)
+
+    def change_username(self, user_id, new_username):
+        for user in self.user_records:
+            if user.username != new_username and user_id == user.user_id:
+                if user.username in self.rented_books:
+                    self.rented_books[new_username] = self.rented_books.pop(
+                        user.username)
+                user.username = new_username
+                return f"Username successfully changed to: {new_username} for userid: {user_id}"
+            if user.username == new_username and user_id == user.user_id:
+                return "Please check again the provided username - it should be different than the username used so far!"
+        return f"There is no user with id = {user_id}!"
