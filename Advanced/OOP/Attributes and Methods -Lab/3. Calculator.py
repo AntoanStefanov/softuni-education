d1aaa93122c51bccb68d5e7b6630fc28d7e4e428
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-03 11:08:52 +0300

    3. Calculator

--- Advanced/OOP/Attributes and Methods -Lab/3. Calculator.py
+++ Advanced/OOP/Attributes and Methods -Lab/3. Calculator.py
@@ -6,12 +6,27 @@ class Calculator:
 
     @staticmethod
     def multiply(*args):
-        print(type(args))
         res = 1
         for n in args:
             res *= n
         return res
 
+    @staticmethod
+    def divide(*args):
+        res = args[0]
+        for i in range(1, len(args)):
+            res = res / args[i]
+        return res
+
+    @staticmethod
+    def subtract(*args):
+        res = args[0]
+        for i in range(1, len(args)):
+            res = res - args[i]
+        return res
+
 
 print(Calculator.add(5, 10, 4))
 print(Calculator.multiply(1, 2, 3, 5))
+print(Calculator.divide(100, 2))
+print(Calculator.subtract(90, 20, -50, 43, 7))
