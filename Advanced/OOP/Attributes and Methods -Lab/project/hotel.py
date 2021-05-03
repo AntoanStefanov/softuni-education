6b7d2fa924b36352ff696930a30d22826bbc0fb2
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-03 14:42:54 +0300

    Class Hotel

---
+++ Advanced/OOP/Attributes and Methods -Lab/project/hotel.py
@@ -0,0 +1,47 @@
+from room import Room
+
+
+class Hotel:
+    def __init__(self, name):
+        self.name = name
+        self.rooms = []
+        self.guests = 0
+
+    @classmethod
+    def from_stars(cls, stars):
+        name = f'{stars} stars Hotel'
+        return cls(name)
+
+    def add_room(self, room: Room):
+        self.rooms.append(room)
+
+    def take_room(self, room_number, people):
+        for room in self.rooms:
+            if room.number == room_number:
+                room.take_room(people)
+
+    def free_room(self, room_number):
+        for room in self.rooms:
+            if room.number == room_number:
+                room.free_room()
+
+    def print_status(self):
+        pass
+
+
+hotel = Hotel.from_stars(5)
+
+first_room = Room(1, 3)
+second_room = Room(2, 2)
+third_room = Room(3, 1)
+
+hotel.add_room(first_room)
+hotel.add_room(second_room)
+hotel.add_room(third_room)
+
+hotel.take_room(1, 4)
+hotel.take_room(1, 2)
+hotel.take_room(3, 1)
+hotel.take_room(3, 1)
+
+hotel.print_status()
