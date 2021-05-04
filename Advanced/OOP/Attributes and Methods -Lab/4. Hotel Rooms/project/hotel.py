4d55a263572359181f7953f791ce322c7c79b140
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-04 13:27:27 +0300

    Class Hotel

---
+++ Advanced/OOP/Attributes and Methods -Lab/4. Hotel Rooms/project/hotel.py
@@ -0,0 +1,41 @@
+class Hotel:
+    def __init__(self, name):
+        self.name = name
+        self.rooms = []
+        self.guests = 0
+
+    @classmethod
+    def from_stars(cls, stars):
+        name = f'{stars} stars Hotel'
+        return cls(name)
+
+    def add_room(self, room):
+        self.rooms.append(room)
+
+    def find_room(self, room_number):
+        for room in self.rooms:
+            if room.number == room_number:
+                return room
+
+    def take_room(self, room_number, people):
+        room = self.find_room(room_number)
+        room.take_room(people)
+
+    def free_room(self, room_number):
+        room = self.find_room(room_number)
+        room.free_room()
+
+    def print_status(self):
+
+        taken = []
+        free = []
+        for room in self.rooms:
+            if room.is_taken:
+                taken.append(room.number)
+                self.guests += room.guests
+            else:
+                free.append(room.number)
+
+        print(f"Hotel {self.name} has {self.guests} total guests")
+        print(f"Free rooms: {', '.join([str(n) for n in free])}")
+        print(f"Taken rooms: {', '.join([str(n) for n in taken])}")
