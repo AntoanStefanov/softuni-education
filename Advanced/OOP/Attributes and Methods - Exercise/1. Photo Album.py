12b5b00a5b3ba20734c21723b5123fbd7a348607
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-05 09:51:14 +0300

    1. Photo Album

--- Advanced/OOP/Attributes and Methods - Exercise/1. Photo Album.py
+++ Advanced/OOP/Attributes and Methods - Exercise/1. Photo Album.py
@@ -3,18 +3,26 @@ class PhotoAlbum:
         self.pages = pages
         self.photos = [[] for i in range(pages)]
 
+    @classmethod
+    def from_photos_count(cls, photos_count: int):
+        return cls(photos_count // 4)
+
+
     def add_photo(self, label):
         for p_index, page in enumerate(self.photos):
-            print(page)
             if len(page) < 4:
                 page.append(label)
                 slot_index = page.index(label)
                 return f"{label} photo added successfully on page {p_index + 1} slot {slot_index + 1}"
         return "No more free spots"
+
     def display(self):
+        res = '-----------\n'
         for page in self.photos:
-            pass
-            
+            photos = [photos.append('[]') for _ in page]    
+            res += ' '.join(photos)
+            res += '\n-----------\n'
+        return res
 
 
 album = PhotoAlbum(2)
