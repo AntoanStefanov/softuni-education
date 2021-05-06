12eed0ef0caa95454b70d2f9260da43547efa87f
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-06 17:40:48 +0300

    Document Management

--- Advanced/OOP/Attributes and Methods - Exercise/3. Document Management/3. Document Management.py
+++ Advanced/OOP/Attributes and Methods - Exercise/3. Document Management/3. Document Management.py
@@ -22,3 +22,40 @@ class Category:
 
     def __repr__(self):
         return f"Category {self.id}: {self.name}"
+
+
+class Document:
+    def __init__(self, id, category_id, topic_id, file_name):
+        self.id = id
+        self.category_id = category_id
+        self.topic_id = topic_id
+        self.file_name = file_name
+        self.tags = []
+
+    @classmethod
+    def from_instances(cls, id: int, category: Category, topic: Topic, file_name: str):
+        return cls(id, category.id, topic.id, file_name)
+
+    def add_tag(self, tag_content):
+        if tag_content not in self.tags:
+            self.tags.append(tag_content)
+
+    def remove_tag(self, tag_content):
+        if tag_content in self.tags:
+            self.tags.remove(tag_content)
+
+    def edit(self, new_file_name):
+        self.file_name = new_file_name
+
+    def __repr__(self):
+        return f"Document {self.id}: {self.file_name}; category {self.category_id}, topic {self.topic_id}, tags: {', '.join(self.tags)}"
+
+class Storage:
+    def __init__(self):
+        self.categories = []
+        self.topics = []
+        self.documents = []
+
+    def add_category(self, category):
+        if category not in self.categories:
+            self.categories.append(category)
