3a0206d658d670806437f02fe538c76b8cc58a2f
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-07 21:55:49 +0300

    4. Gym

--- Advanced/OOP/Attributes and Methods - Exercise/4. Gym/4. Gym.py
+++ Advanced/OOP/Attributes and Methods - Exercise/4. Gym/4. Gym.py
@@ -27,7 +27,25 @@ class Equipment:
     def __repr__(self):
         return f"Equipment <{self.equipment_id}> {self.name}"
 
+class ExercisePlan:
+    exercise_plan_id = 0
+    def __init__(self, trainer_id, equipment_id, duration): 
+        ExercisePlan.exercise_plan_id += 1
+        self.trainer_id = trainer_id
+        self.equipment_id = equipment_id
+        self.duration = duration # in minutes
+        self.exercise_plan_id = ExercisePlan.exercise_plan_id
+    
+    @classmethod
+    def from_hours(cls, trainer_id:int, equipment_id:int, hours:int):
+        cls(trainer_id, equipment_id, hours * 60)
 
+    @staticmethod
+    def get_next_id():
+        return ExercisePlan.exercise_plan_id + 1
+
+    def __repr__(self):
+        return f"Plan <{self.exercise_plan_id}> with duration {self.duration} minutes" 
 customer = Customer("John", "Maple Street", "john.smith@gmail.com")
 print(customer)
 print(customer.get_next_id())
