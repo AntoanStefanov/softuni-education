e906df52589b60f29d26946d3192bb3ee3bc85da
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-11 11:23:41 +0300

    1. Person

---
+++ Advanced/OOP/Encapsulation - Lab/1. Person.py
@@ -0,0 +1,18 @@
+class Person:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def age(self):
+        return self.__age
+
+
+person = Person('Tony', 12)
+
+print(person.name)
+print(person.age)
