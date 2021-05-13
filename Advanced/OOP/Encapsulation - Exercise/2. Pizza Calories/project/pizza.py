83f6c6e743f3c2afa9bb12f38313d3752609d531
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-13 13:54:36 +0300

    Class Pizza

---
+++ Advanced/OOP/Encapsulation - Exercise/2. Pizza Calories/project/pizza.py
@@ -0,0 +1,52 @@
+class Pizza:
+
+    def __init__(self, name, dough, toppings_capacity):
+        self.name = name
+        self.dough = dough
+        self.toppings_capacity = toppings_capacity
+        # It ll contain the topping type as a key and the topping's weight as a value.
+        self.toppings = {}
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name = value
+
+    @property
+    def dough(self):
+        return self.__dough
+
+    @dough.setter
+    def dough(self, value):
+        self.__dough = value
+
+    @property
+    def toppings_capacity(self):
+        return self.__toppings_capacity
+
+    @toppings_capacity.setter
+    def toppings_capacity(self, value):
+        self.__toppings_capacity = value
+
+    @property
+    def toppings(self):
+        return self.__toppings
+
+    @toppings.setter
+    def toppings(self, value):
+        self.__toppings = value
+
+    def add_topping(self, topping):
+        if len(self.toppings) < self.toppings_capacity:
+            if topping.topping_type in self.toppings:
+                self.toppings[topping.topping_type] += topping.weight
+            else:
+                self.toppings[topping.topping_type] = topping.weight
+        else:
+            raise ValueError("Not enough space for another topping")
+
+    def calculate_total_weight(self):
+        return sum(self.toppings.values()) + self.dough.weight
