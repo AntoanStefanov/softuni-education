decd51af7c109cbcfe7df9e436924ea278072e4d
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-13 13:54:08 +0300

    Class Zoo

---
+++ Advanced/OOP/Encapsulation - Exercise/1. Wild Cat Zoo/project/zoo.py
@@ -0,0 +1,99 @@
+class Zoo:
+
+    def __init__(self, name, budget, animal_capacity, workers_capacity):
+        self.__budget = budget
+        self.__animal_capacity = animal_capacity
+        self.__workers_capacity = workers_capacity
+        self.name = name
+        self.animals = []
+        self.workers = []
+
+    def total_salary_cost(self):
+        total_cost = 0
+        for worker in self.workers:
+            total_cost += worker.salary
+        return total_cost
+
+    def get_all_animals_needs(self):
+        all_animals_needs = 0
+        for animal in self.animals:
+            all_animals_needs += animal.get_needs()
+        return all_animals_needs
+
+    def add_animal(self, animal, price):
+        if self.__budget - price < 0:
+            return "Not enough budget"
+        if len(self.animals) >= self.__animal_capacity:
+            return "Not enough space for animal"
+
+        self.animals.append(animal)
+        self.__budget -= price
+        return f"{animal.name} the {type(animal).__name__} added to the zoo"
+
+    def hire_worker(self, worker):
+        if len(self.workers) >= self.__workers_capacity:
+            return "Not enough space for worker"
+        self.workers.append(worker)
+        return f"{worker.name} the {type(worker).__name__} hired successfully"
+
+    def fire_worker(self, worker_name):
+        for worker in self.workers:
+            if worker.name == worker_name:
+                self.workers.remove(worker)
+                return f"{worker_name} fired successfully"
+        return f"There is no {worker_name} in the zoo"
+
+    def pay_workers(self):
+        total_salary_cost = self.total_salary_cost()
+        if self.__budget >= total_salary_cost:
+            self.__budget -= total_salary_cost
+            return f"You payed your workers. They are happy. Budget left: {self.__budget}"
+        return "You have no budget to pay your workers. They are unhappy"
+
+    def tend_animals(self):
+        all_animals_needs = self.get_all_animals_needs()
+        if self.__budget >= all_animals_needs:
+            self.__budget -= all_animals_needs
+            return f"You tended all the animals. They are happy. Budget left: {self.__budget}"
+        return "You have no budget to tend the animals. They are unhappy."
+
+    def profit(self, amount):
+        self.__budget += amount
+
+    def animals_status(self):
+
+        lions = [animal for animal in self.animals if type(
+            animal).__name__ == 'Lion']
+        tigers = [animal for animal in self.animals if type(
+            animal).__name__ == 'Tiger']
+        cheetahs = [animal for animal in self.animals if type(
+            animal).__name__ == 'Cheetah']
+
+        status = f'You have {len(self.animals)} animals\n'
+        status += f'----- {len(lions)} Lions:\n'
+        status += '\n'.join([f'{lion}' for lion in lions])
+        status += f'\n----- {len(tigers)} Tigers:\n'
+        status += '\n'.join([f'{tigers}' for tigers in tigers])
+        status += f'\n----- {len(cheetahs)} Cheetahs:\n'
+        status += '\n'.join([f'{ch}' for ch in cheetahs])
+
+        return status
+
+    def workers_status(self):
+
+        keepers = [person for person in self.workers if type(
+            person).__name__ == 'Keeper']
+        caretakers = [person for person in self.workers if type(
+            person).__name__ == 'Caretaker']
+        vets = [person for person in self.workers if type(
+            person).__name__ == 'Vet']
+
+        status = f'You have {len(self.workers)} workers\n'
+        status += f'----- {len(keepers)} Keepers:\n'
+        status += '\n'.join([f'{keeper}' for keeper in keepers])
+        status += f'\n----- {len(caretakers)} Caretakers:\n'
+        status += '\n'.join([f'{caretaker}' for caretaker in caretakers])
+        status += f'\n----- {len(vets)} Vets:\n'
+        status += '\n'.join([f'{vet}' for vet in vets])
+
+        return status
