9a3f7e8c40ea07a87f66f15f3094b6cca80d1560
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-21 13:29:41 +0300

    5. Truck Tour

--- Advanced/Advanced/Lists as Stacks and Queues - Exercise/5. Truck Tour.py
+++ Advanced/Advanced/Lists as Stacks and Queues - Exercise/5. Truck Tour.py
@@ -34,3 +34,31 @@ while pumps:
                 break
 
 print(original_pumps.index(get_this_index))
+
+
+################ SECOND SOLUTION #############
+
+number_of_pumps = int(input())
+
+
+pumps = deque()
+
+for pump in range(number_of_pumps):
+    petrol, next_pump_distance = [int(x) for x in input().split()]
+    pumps.append(petrol - next_pump_distance)
+
+initial_pumps = pumps.copy()
+passed_pumps = deque()
+tank = 0
+
+while pumps:
+    pump = pumps.popleft()
+    passed_pumps.append(pump)
+    tank += pump
+    if tank < 0:
+        tank = 0
+        while passed_pumps:
+            pumps.append(passed_pumps.popleft())
+        passed_pumps = deque()
+
+print(initial_pumps.index(passed_pumps[0]))
