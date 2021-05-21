b80ae9e8e8915b835e0a393eeeba9285b4cb73ba
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-21 13:29:33 +0300

    4. Fashion Boutique

--- Advanced/Advanced/Lists as Stacks and Queues - Exercise/4. Fashion Boutique.py
+++ Advanced/Advanced/Lists as Stacks and Queues - Exercise/4. Fashion Boutique.py
@@ -18,3 +18,29 @@ while box:
     elif len(box) == 0:  # Когато попнем последната дреха е нужен рак също, иначе няма да се остави
         racks += 1
 print(racks)
+
+############## SECOND TIME ############# 
+box_of_clothes = [int(clothes) for clothes in input().split()]  # stack
+one_rack_capacity = int(input())
+
+
+clothes_on_current_rack = 0
+racks = 0
+
+
+while box_of_clothes:
+    current_cloth = box_of_clothes.pop()
+    clothes_on_current_rack += current_cloth
+    if clothes_on_current_rack == one_rack_capacity:
+        clothes_on_current_rack = 0
+        racks += 1
+    elif clothes_on_current_rack > one_rack_capacity:
+        racks += 1
+        clothes_on_current_rack = current_cloth
+
+        
+if clothes_on_current_rack:
+    racks += 1
+
+
+print(racks)
