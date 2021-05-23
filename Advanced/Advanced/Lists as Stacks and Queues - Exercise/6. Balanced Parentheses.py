92f55e5f2b2b485c951d4bb26dc26cc6e2de6e2f
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-23 09:13:30 +0300

    6. Balanced Parentheses

--- Advanced/Advanced/Lists as Stacks and Queues - Exercise/6. Balanced Parentheses.py
+++ Advanced/Advanced/Lists as Stacks and Queues - Exercise/6. Balanced Parentheses.py
@@ -1,24 +1,46 @@
-parentheses = input()
+from collections import deque
+brackets = input()
 open_parentheses = []  # stack
 
-for parenthesis in parentheses:
-    
-    if not len(parentheses) % 2 == 0:
+for skoba in brackets:
+
+    if not len(brackets) % 2 == 0:
         print('NO')
         break
-    if parenthesis == '(' or parenthesis == '{' or parenthesis == '[':
-        open_parentheses.append(parenthesis)
+    if skoba == '(' or skoba == '{' or skoba == '[':
+        open_parentheses.append(skoba)
     else:
         last_open_parentheses = open_parentheses.pop()
-        pair = last_open_parentheses + parenthesis
-        if pair == '()':
-            continue
-        elif pair == '{}':
-            continue
-        elif pair == '[]':
+        pair = last_open_parentheses + skoba
+        if pair == '()' or pair == '{}' or pair == '[]':
             continue
         else:
             print('NO')
             break
 else:
     print('YES')
+
+########## SECOND SOLUTION ############
+
+
+brackets = deque(input())
+opening_brackets = deque()
+closing_brackets = deque()
+if len(brackets) % 2 == 1:
+    print('NO')
+else:
+    while brackets:
+        bracket = brackets.popleft()
+
+        if bracket == '(' or bracket == '{' or bracket == '[':
+            opening_brackets.append(bracket)
+        else:
+            pair = opening_brackets.pop() + bracket
+
+            if pair == '()' or pair == '[]' or pair == '{}':
+                continue
+            else:
+                print('NO')
+                break
+    else:
+        print('YES')
