42cf346d0ab147362220266e91c806b3445704d8
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-25 18:07:26 +0300

    1. Unique Username

--- Advanced/Advanced/Tuples and Sets - Exercise/1.Unique Username.py
+++ Advanced/Advanced/Tuples and Sets - Exercise/1.Unique Username.py
@@ -5,3 +5,10 @@ for _ in range(n):
     usernames.add(username)
 
 print('\n'.join(usernames))
+
+
+### SECOND SOLUTION ### 
+
+n = int(input())
+unique_names = set([input() for _ in range(n)])
+print('\n'.join(unique_names))
