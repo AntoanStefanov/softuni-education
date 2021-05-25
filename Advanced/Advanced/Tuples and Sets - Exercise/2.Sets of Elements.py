86b27bfe8afec76d092a61d8eecbe86245962d1b
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-25 18:07:34 +0300

    2. Sets of Elements

--- Advanced/Advanced/Tuples and Sets - Exercise/2.Sets of Elements.py
+++ Advanced/Advanced/Tuples and Sets - Exercise/2.Sets of Elements.py
@@ -13,3 +13,19 @@ for _ in range(numbers_of_second_set):
 unique_elements_in_both_sets = first_set & second_set
 
 [print(num) for num in unique_elements_in_both_sets]
+
+
+########### SECOND SOLUTION #################
+
+first_set_len, second_set_len = [int(x) for x in input().split()]
+
+first_set = set()
+second_set = set()
+
+[first_set.add(int(input())) for _ in range(first_set_len)]
+
+[second_set.add(int(input())) for _ in range(second_set_len)]
+
+duplicates_of_both_sets = first_set.intersection(second_set)
+
+[print(num) for num in duplicates_of_both_sets]
