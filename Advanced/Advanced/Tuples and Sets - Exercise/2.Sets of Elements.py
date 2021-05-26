a909d79a569ef64cb078739d72590bacb04cf3d3
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-26 13:37:51 +0300

    2. Sets of Elements

--- Advanced/Advanced/Tuples and Sets - Exercise/2.Sets of Elements.py
+++ Advanced/Advanced/Tuples and Sets - Exercise/2.Sets of Elements.py
@@ -1,23 +1,23 @@
-first_set = set()
-second_set = set()
+# first_set = set()
+# second_set = set()
 
-numbers_of_first_set, numbers_of_second_set = [int(n) for n in input().split()]
+# numbers_of_first_set, numbers_of_second_set = [int(n) for n in input().split()]
 
-for _ in range(numbers_of_first_set):
-    num = int(input())
-    first_set.add(num)
-for _ in range(numbers_of_second_set):
-    num = int(input())
-    second_set.add(num)
+# for _ in range(numbers_of_first_set):
+#     num = int(input())
+#     first_set.add(num)
+# for _ in range(numbers_of_second_set):
+#     num = int(input())
+#     second_set.add(num)
 
-unique_elements_in_both_sets = first_set & second_set
+# unique_elements_in_both_sets = first_set & second_set
 
-[print(num) for num in unique_elements_in_both_sets]
+# [print(num) for num in unique_elements_in_both_sets]
 
 
 ########### SECOND SOLUTION #################
 
-first_set_len, second_set_len = [int(x) for x in input().split()]
+first_set_len, second_set_len = tuple([int(x) for x in input().split()])
 
 first_set = set()
 second_set = set()
