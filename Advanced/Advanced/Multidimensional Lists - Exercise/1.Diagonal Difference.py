bfca8b1a1bc6ee5687ee2dc1a4eb609152147c9a
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-30 09:29:47 +0300

    1.Diagonal Difference

--- Advanced/Advanced/Multidimensional Lists - Exercise/1.Diagonal Difference.py
+++ Advanced/Advanced/Multidimensional Lists - Exercise/1.Diagonal Difference.py
@@ -17,3 +17,18 @@ for row in range(square_matrix_size-1, -1, -1):
 
 
 print(abs(primary_diagonal_sum - secondary_diagonal_sum))
+
+
+# Second time #
+
+n = int(input())
+
+matrix = [[int(num) for num in input().split()] for _ in range(n)]
+
+primary_diagonal_sum = 0
+secondary_diagonal_sum = 0
+for i in range(n):
+    primary_diagonal_sum += matrix[i][i]
+    secondary_diagonal_sum += matrix[i][n - 1 - i]
+
+print(abs(primary_diagonal_sum - secondary_diagonal_sum))
