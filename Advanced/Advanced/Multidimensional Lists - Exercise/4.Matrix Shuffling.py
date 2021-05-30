f72f580ec1f938d38cec3eb3e392f1adbb2fb261
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-30 09:30:07 +0300

    4.Matrix Shuffling

--- Advanced/Advanced/Multidimensional Lists - Exercise/4.Matrix Shuffling.py
+++ Advanced/Advanced/Multidimensional Lists - Exercise/4.Matrix Shuffling.py
@@ -17,8 +17,8 @@ while True:
     command = data[0]
 
     if command == 'swap':
-        if len(data) == 5: 
-           
+        if len(data) == 5:
+
             first_row_index, first_col_index, second_row_index, second_col_index = data[1:]
 
             first_row_index = int(first_row_index)
@@ -43,3 +43,28 @@ while True:
             print('Invalid input!')
     else:
         print('Invalid input!')
+
+
+############### SECOND TIME  ########
+
+rows, columns = [int(x) for x in input().split()]
+matrix = []
+for _ in range(rows):
+    matrix.append(input().split())
+
+while True:
+    command = input()
+    if command == 'END':
+        break
+    command = command.split()
+    if command[0] == 'swap' and len(command) == 5:
+        indexes = [int(x) for x in command[1:]]
+        if 0 <= indexes[0] < rows and 0 <= indexes[2] < rows and 0 <= indexes[1] < columns and 0 <= indexes[3] < columns:
+            matrix[indexes[0]][indexes[1]], matrix[indexes[2]][indexes[3]
+                                                               ] = matrix[indexes[2]][indexes[3]], matrix[indexes[0]][indexes[1]]
+            for row in matrix:
+                print(' '.join(row))
+
+            continue
+
+    print('Invalid input!')
