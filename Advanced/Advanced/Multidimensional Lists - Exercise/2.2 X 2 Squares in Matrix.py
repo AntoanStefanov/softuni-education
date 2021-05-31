8bbda168d4dc7729c5d600ffc7ae6fab6de59eeb
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-31 10:59:17 +0300

    2. 2 X 2 Squares in Matrix

--- Advanced/Advanced/Multidimensional Lists - Exercise/2.2 X 2 Squares in Matrix.py
+++ Advanced/Advanced/Multidimensional Lists - Exercise/2.2 X 2 Squares in Matrix.py
@@ -1,6 +1,5 @@
 rows, columns = [int(x) for x in input().split()]
 
-
 matrix = []
 
 
@@ -26,3 +25,32 @@ for row in range(rows - 1):
         # print(next_row_current_char, next_row_next_char)
         # print()
 print(counter)
+
+
+# Second Time #
+rows, columns = [int(x) for x in input().split()]
+matrix = []
+for r in range(rows):
+    matrix.append(input().split())
+
+squares = 0
+# -1 Защото квадрата си е 2х2 и няма смисъл да чеквам последния 1 ред, защото излизат от матрицата
+for row in range(rows - 1):
+    # -1 Защото квадрата си е 2х2 и няма смисъл да чеквам последната 1 колона, защото излизат от матрицата
+    for col in range(columns - 1):
+        current_char = matrix[row][col]
+        # Possible right
+        if not current_char == matrix[row][col + 1]:
+            continue
+
+        # Possible diagonal
+        if not current_char == matrix[row + 1][col + 1]:
+            continue
+
+        # Possible down
+        if not current_char == matrix[row + 1][col]:
+            continue
+
+        squares += 1
+
+print(squares)
