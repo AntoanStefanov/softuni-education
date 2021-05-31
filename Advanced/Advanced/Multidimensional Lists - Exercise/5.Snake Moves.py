e8a5b07f92c24c6ddef0632adbbc75feb7f1938c
AntoanStefanov <antoine.stefanov@gmail.com>
2021-05-31 10:59:40 +0300

    5. Snake Moves

--- Advanced/Advanced/Multidimensional Lists - Exercise/5.Snake Moves.py
+++ Advanced/Advanced/Multidimensional Lists - Exercise/5.Snake Moves.py
@@ -1,17 +1,19 @@
 from collections import deque
+
 rows, columns = [int(x) for x in input().split()]
 
 snake = deque(input())
-
-matrix = [[] for _ in range(rows)]
-
+matrix = []
 
 for row in range(rows):
+    matrix.append([])
     for column in range(columns):
-        next_element = snake.popleft()
-        matrix[row].append(next_element)
-        snake.append(next_element)
+        char = snake.popleft()
+        matrix[row].append(char)
+        snake.append(char)
     if row % 2 == 1:
         matrix[row].reverse()
+
 for row in matrix:
+
     print(''.join(row))
