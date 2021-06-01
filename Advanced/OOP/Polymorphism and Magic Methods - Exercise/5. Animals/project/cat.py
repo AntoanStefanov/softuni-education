a8c636523fd4bf1498b57e21f6fa692f8d81056b
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-01 11:43:20 +0300

    Class Cat

---
+++ Advanced/OOP/Polymorphism and Magic Methods - Exercise/5. Animals/project/cat.py
@@ -0,0 +1,10 @@
+from .animal import Animal
+
+
+class Cat(Animal):
+
+    @staticmethod
+    def make_sound(): return "Meow meow!"
+
+    def __repr__(self):
+        return f"This is {self.name}. {self.name} is a {self.age} year old {self.gender} {type(self).__name__}"
