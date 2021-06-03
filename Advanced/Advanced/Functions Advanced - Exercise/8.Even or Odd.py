d03c6d9c713dee2c16d3315814ed1515c5bfd90a
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-03 11:21:59 +0300

    8. Even or Odd

--- Advanced/Advanced/Functions Advanced - Exercise/8.Even or Odd.py
+++ Advanced/Advanced/Functions Advanced - Exercise/8.Even or Odd.py
@@ -9,3 +9,9 @@ def even_odd(*args):
 
 print(even_odd(1, 2, 3, 4, 5, 6, "even"))
 print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
+
+######### SECOND TIME ######
+def even_odd(*args):
+    if args[-1] == 'odd':
+        return list(filter(lambda x: x % 2 == 1, args[:-1]))
+    return list(filter(lambda x: x % 2 == 0, args[:-1]))
