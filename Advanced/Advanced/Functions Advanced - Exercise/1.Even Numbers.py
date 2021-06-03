1c4f00a859b390c5dfff311318e72ca25b07cda9
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-03 11:21:43 +0300

    1. Even Numbers

--- Advanced/Advanced/Functions Advanced - Exercise/1.Even Numbers.py
+++ Advanced/Advanced/Functions Advanced - Exercise/1.Even Numbers.py
@@ -3,3 +3,8 @@ numbers = [int(x) for x in input().split()]
 even_nums = filter(lambda x: x % 2 == 0, numbers)
 
 print(list(even_nums))
+
+# Second time through
+
+ints = [int(x) for x in input().split()]
+print(list(filter(lambda x: x % 2 == 0, ints)))
