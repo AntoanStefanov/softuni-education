f8d37e22cbe34f0e53366dc6381bc3dce45d5545
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-03 11:21:52 +0300

    4. Negative vs Positive

--- Advanced/Advanced/Functions Advanced - Exercise/4.Negative vs Positive.py
+++ Advanced/Advanced/Functions Advanced - Exercise/4.Negative vs Positive.py
@@ -10,3 +10,21 @@ if abs(sum(negatives_nums)) > sum(positives_nums):
     print('The negatives are stronger than the positives')
 else:
     print("The positives are stronger than the negatives")
+
+
+############# Second Timee ####
+
+ints = [int(x) for x in input().split()]
+
+negatives = [num for num in ints if num < 0]
+positives = [num for num in ints if num > 0]
+
+neg_sum = sum(negatives)
+pos_sum = sum(positives)
+
+print(neg_sum)
+print(pos_sum)
+
+[print('The negatives are stronger than the positives')
+ if abs(neg_sum) > pos_sum else
+ print('The positives are stronger than the negatives')]
