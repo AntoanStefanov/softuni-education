c16edcdd94248ce010e741477a7a19652d05a3d8
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-04 11:52:31 +0300

    11. Age Assignment

--- Advanced/Advanced/Functions Advanced - Exercise/11.Age Assignment.py
+++ Advanced/Advanced/Functions Advanced - Exercise/11.Age Assignment.py
@@ -8,3 +8,12 @@ def age_assignment(*args, **kwargs):
 
 print(age_assignment("Peter", "George", G=26, P=19))
 print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
+
+
+########### SECOND TIME #################
+
+def age_assignment(*args, **kwargs):
+    return {name: kwargs[name[0]] for name in args}
+
+
+print(age_assignment("Peter", "George", G=26, P=19))
