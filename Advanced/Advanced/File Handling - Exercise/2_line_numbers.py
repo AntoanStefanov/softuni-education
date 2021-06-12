7c749121e4889d71f3a371baf706a784cbe20f7d
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-12 10:07:51 +0300

    2_line_numbers

--- Advanced/Advanced/File Handling - Exercise/2_line_numbers.py
+++ Advanced/Advanced/File Handling - Exercise/2_line_numbers.py
@@ -8,13 +8,22 @@ def get_n(pattern, sentence):
     return len(re.findall(pattern, sentence))
 
 
-with open('text.txt', 'r') as file:
-    sentences = file.readlines()
+def prepare_output(sentences):
     output = ''
     for index, sentence in enumerate(sentences, 1):
         n_letters = get_n(letters_pattern, sentence)
         n_punctuation_marks = get_n(punctuation_marks_pattern, sentence)
-        output += f'Line {index}: {sentence.strip()} ({n_letters})({n_punctuation_marks})\n'
+        output += f'Line {index}: {sentence} ({n_letters})({n_punctuation_marks})\n'
+    return output[:-1]
 
-with open('output.txt', 'w') as file:
-    file.write(output)
+
+def start_program():
+    with open('text.txt', 'r') as file:
+        sentences = file.read().split('\n')
+        output = prepare_output(sentences)
+
+    with open('output.txt', 'w') as file:
+        file.write(output)
+
+
+start_program()
