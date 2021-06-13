7ca3e8eb7d6464b536e9fee459c0634d306d8b15
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-13 17:19:43 +0300

    Even Lines

--- Advanced/Advanced/File Handling - Exercise/1_even_lines.py
+++ Advanced/Advanced/File Handling - Exercise/1_even_lines.py
@@ -2,11 +2,7 @@ import re
 
 
 def replace_and_reverse_sentence(sentence):
-    # the only special characters or metacharacters inside a character class are
-    #  the closing bracket ],
-    #  the backslash \,
-    #  the caret ^,
-    #  and the hyphen -
+
     sentence = re.sub(r'[-,.!?]', '@', sentence)
     words = sentence.split()
     return ' '.join(words[::-1])
@@ -18,3 +14,11 @@ with open('text.txt', 'r') as file:
         if index % 2 == 0:
             edited_sentence = replace_and_reverse_sentence(sentence)
             print(edited_sentence)
+
+
+# Note:
+    # the only special characters or metacharacters inside a character class are:
+    # the closing bracket ],
+    # the backslash \,
+    # the caret ^,
+    # and the hyphen - (no need to escape if it's at the beginning or the end).
