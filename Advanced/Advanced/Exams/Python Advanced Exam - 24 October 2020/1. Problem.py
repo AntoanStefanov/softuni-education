7371e830d6a6f19eeb0a4206f7e7af5d24be9806
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-14 15:45:42 +0300

    1. Problem

--- Advanced/Advanced/Exams/Python Advanced Exam - 24 October 2020/1. Problem.py
+++ Advanced/Advanced/Exams/Python Advanced Exam - 24 October 2020/1. Problem.py
@@ -17,3 +17,28 @@ while True:
 
 
 print(clock_cycles)
+
+
+########### SECOND HARDER WAY , bcs the exam is near, I panicked, and didnt solve it properly ####
+
+jobs = deque([int(x) for x in input().split(', ')])
+wanted_job_index = int(input())
+wanted_job = jobs[wanted_job_index]
+clock_cycles = 0
+
+similar_jobs = [job for job in jobs if job == wanted_job]
+
+while True:
+
+    job_to_complete = min(jobs)
+    current_job = jobs.popleft()
+    if job_to_complete == current_job:
+        clock_cycles += job_to_complete
+        if job_to_complete == wanted_job:
+            if len(similar_jobs) > 1:
+                similar_jobs.pop()
+            else:
+                print(clock_cycles)
+                break
+    else:
+        jobs.append(current_job)
