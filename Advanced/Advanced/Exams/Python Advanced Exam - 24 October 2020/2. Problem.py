e33ddb043d601663bae83c9d13e1d2a481b5f452
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-15 12:14:08 +0300

    2. Problem

--- Advanced/Advanced/Exams/Python Advanced Exam - 24 October 2020/2. Problem.py
+++ Advanced/Advanced/Exams/Python Advanced Exam - 24 October 2020/2. Problem.py
@@ -130,9 +130,6 @@ else:
 
 ######## SECOND WAY ##########
 
-n = 8
-
-
 def read_matrix(n):
     matrix = []
     for row in range(n):
@@ -152,35 +149,112 @@ def is_index_valid(r, c, n):
     return 0 <= r < n and 0 <= c < n
 
 
+def is_queen_on_line(row, col, position):
+    while True:
+        potential_row, potential_col = position[0] + \
+            row, position[1] + col
+        if is_index_valid(potential_row, potential_col, n):
+            row, col = potential_row, potential_col
+
+            if matrix[potential_row][potential_col] == 'Q':
+                return [potential_row, potential_col]
+        else:
+            return
+
+
+n = 8
 matrix = read_matrix(n)
 king_position = find_king_position(matrix)
 positions = [
     [0, -1],  # LEFT
     [-1, -1],  # LEFT UP
     [-1, 0],  # UP
-    [-1, +1],  # RIGHT UP
-    [0, +1],  # RIGHT
-    [+1, +1],  # RIGHT DOWN
+    [-1, 1],  # RIGHT UP
+    [0, 1],  # RIGHT
+    [1, 1],  # RIGHT DOWN
     [1, 0],  # DOWN
-    [+1, -1],  # LEFT DOWN
+    [1, -1],  # LEFT DOWN
 ]
 queens = []
 for position in positions:
     row, col = king_position[0], king_position[1]
-    while True:
-        potential_row, potential_col = position[0] + \
-            row, position[1] + col
+    queen = is_queen_on_line(row, col, position)
+    if queen:
+        queens.append(queen)
 
-        if is_index_valid(potential_row, potential_col, n):
-            if matrix[potential_row][potential_col] == 'Q':
-                queens.append([potential_row, potential_col])
-                break
-            else:
-                row, col = potential_row, potential_col
-        else:
-            break
 if queens:
     for queen in queens:
         print(queen)
 else:
     print("The king is safe!")
+
+
+##### DONCHO ####
+
+def read_matrix(n):
+    matrix = []
+    for row in range(n):
+        matrix.append([])
+        matrix[row] = input().split()
+    return matrix
+
+
+def find_king_position(matrix):
+    for row_index in range(len(matrix)):
+        if 'K' in matrix[row_index]:
+            column_index = matrix[row_index].index('K')
+            return row_index, column_index
+
+
+def in_range(value, max_value):
+    return 0 <= value < max_value
+
+
+def search_with_deltas(board, king, deltas):
+    rows_count = len(board)
+    column_count = len(board[0])
+    delta_row, delta_col = deltas
+    row_index, col_index = king
+
+    while True:
+        if board[row_index][col_index] == 'Q':
+            return [row_index, col_index]
+        row_index += delta_row
+        col_index += delta_col
+
+        if (not in_range(row_index, rows_count))\
+                or (not in_range(col_index, column_count)):
+            return None
+
+
+def get_capturing_queens(board, king):
+    deltas = [
+        (0, -1),
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+        (1, 0),
+        (1, -1),
+    ]
+    queens = [
+        search_with_deltas(board, king, delta)
+        for delta in deltas
+    ]
+    return [queen for queen in queens if queen is not None]
+
+
+def print_result(queens):
+    if queens:
+        for queen in queens:
+            print(queen)
+    else:
+        print('The king is safe!')
+
+
+n = 8
+board = read_matrix(n)
+king = find_king_position(board)
+queens = get_capturing_queens(board, king)
+print_result(queens)
