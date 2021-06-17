418c73aca09f1f13bce948879f3b29aeda22b416
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-17 13:16:12 +0300

    2. Problem

--- Advanced/Advanced/Exams/Retake Exam - 16 Dec 2020/2. Problem.py
+++ Advanced/Advanced/Exams/Retake Exam - 16 Dec 2020/2. Problem.py
@@ -1,3 +1,77 @@
+######### SECOND TIME #####
+
+
+def read_matrix(n):
+    matrix = []
+    for row in range(n):
+        matrix.append([])
+        matrix[row] = list(input())
+    return matrix
+
+
+def read_directions():
+    n_directions = int(input())
+    directions = []
+    for _ in range(n_directions):
+        directions.append(input())
+    return directions
+
+
+def find_player_position(matrix):
+    for row_index in range(len(matrix)):
+        for col_index in range(len(matrix[0])):
+            if matrix[row_index][col_index] == 'P':
+                return [row_index, col_index]
+
+
+def next_position(player_row, player_col, direction):
+    moves = {
+        'left': (0, -1),
+        'up': (-1, 0),
+        'right': (0, 1),
+        'down': (1, 0)
+    }
+    player_row += moves[direction][0]
+    player_col += moves[direction][1]
+    return [player_row, player_col]
+
+
+def is_position_valid(row, col, n):
+    return 0 <= row < n and 0 <= col < n
+
+
+def check_player_position(matrix, row, col):
+    if not matrix[row][col] == '-':
+        letter = matrix[row][col]
+        matrix[row][col] = 'P'
+        return letter
+    return None
+
+
+text = input()
+n = int(input())
+matrix = read_matrix(n)
+directions = read_directions()
+player_row, player_col = find_player_position(matrix)
+
+for direction in directions:
+    next_potential_position = next_position(player_row, player_col, direction)
+    if is_position_valid(*next_potential_position, n):
+        matrix[player_row][player_col] = '-'
+        player_row, player_col = next_potential_position
+        letter = check_player_position(matrix, player_row, player_col)
+        if letter:
+            text += letter
+    else:
+        if text:
+            text = text[:-1]
+print(text)
+for row in matrix:
+    print(*row, sep='')
+
+
+####### FIRST TIME ########
+
 string = input()
 n = int(input())
 
@@ -17,8 +91,6 @@ for row in range(n):
         if matrix[row][col] == 'P':
             player_row = row
             player_col = col
-
-
 
 
 number_of_commands = len(commands) - 1
