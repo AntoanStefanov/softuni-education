586ff52bea8d1b09af619e3b42cbc7cf6b094635
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-18 14:10:10 +0300

    3. Countdown Iterator

---
+++ Advanced/OOP/Iterators and Generators - Exercise/3. Countdown Iterator.py
@@ -0,0 +1,18 @@
+class countdown_iterator:
+    def __init__(self, count):
+        self.count = count
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count < 0:
+            raise StopIteration()
+        current_value = self.count
+        self.count -= 1
+        return current_value
+
+
+iterator = countdown_iterator(3)
+for item in iterator:
+    print(item, end=" ")
