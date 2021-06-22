bd5e7b29be463edf19ef9377d325ae1756a14c0b
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-22 10:34:25 +0300

    Problem 3

--- Advanced/Advanced/Exams/Exam - 14 February 2021/Problem 3.py
+++ Advanced/Advanced/Exams/Exam - 14 February 2021/Problem 3.py
@@ -1,16 +1,23 @@
 def stock_availability(boxes: list, action, *args):
-    
     if action == 'delivery':
-        pass
+        boxes.extend(args)
     elif action == 'sell':
-        pass
+        if not args:  # no args
+            boxes.pop(0)
+        else:
+            if type(args[0]) == int:
+                sold_boxes = args[0]
+                for _ in range(sold_boxes):
+                    boxes.pop(0)
+            else:
+                for flavor in args:
+                    while flavor in boxes:
+                        boxes.remove(flavor)
+    return boxes
 
 
-
-print(stock_availability(["choco", "vanilla", "banana"], "delivery", "caramel", "berry"))
-print(stock_availability(["chocolate", "vanilla", "banana"], "delivery", "cookie","banana"))
-print(stock_availability(["chocolate", "vanilla", "banana"], "sell"))
-print(stock_availability(["chocolate", "vanilla", "banana"], "sell", 3))
-print(stock_availability(["chocolate", "chocolate", "banana"], "sell", "chocolate"))
-print(stock_availability(["cookie", "chocolate", "banana"], "sell", "chocolate"))
+print(stock_availability(
+    ["chocolate", "chocolate", "banana"], "sell", "chocolate"))
+print(stock_availability(
+    ["cookie", "chocolate", "banana"], "sell", "chocolate"))
 print(stock_availability(["chocolate", "vanilla", "banana"], "sell", "cookie"))
