42299c9466e775437dac2537cc6466616fdbc115
AntoanStefanov <antoine.stefanov@gmail.com>
2021-06-27 08:17:26 +0300

    4. Type Check

---
+++ Advanced/OOP/Decorators - Exercise/4. Type Check.py
@@ -0,0 +1,17 @@
+def type_check(type_to_check):
+    def decorator(func):
+        def wrapper(param):
+            if type(param) == type_to_check:
+                return func(param)
+            return "Bad Type"
+        
+        return wrapper
+
+    return decorator
+
+
+@type_check(int)
+def times2(num):
+    return num*2
+print(times2(2))
+print(times2('Not A Number'))
