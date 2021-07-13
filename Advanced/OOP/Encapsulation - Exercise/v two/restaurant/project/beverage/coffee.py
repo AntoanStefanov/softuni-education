af9dc61388d4e94e8d2d6127abfa2a43a4062e2b
AntoanStefanov <antoine.stefanov@gmail.com>
2021-07-13 09:30:10 +0300

    cofee

---
+++ Advanced/OOP/Encapsulation - Exercise/v two/restaurant/project/beverage/coffee.py
@@ -0,0 +1,15 @@
+from .hot_beverage import HotBeverage
+
+
+class Coffee(HotBeverage):
+    PRICE = 3.50
+    MILLILITERS = 50
+
+    def __init__(self, name, caffeine):
+
+        super().__init__(name, self.PRICE, self.__class__.MILLILITERS)
+        self.__caffeine = caffeine
+
+    @property
+    def caffeine(self):
+        return self.__caffeine
