44a7658425873d10d7d4d2814ed67914dd320f41
AntoanStefanov <antoine.stefanov@gmail.com>
2021-07-16 12:17:49 +0300

    trainer

--- Advanced/OOP/Attributes and Methods - Exercise/version_two/gym/project/trainer.py
+++ Advanced/OOP/Attributes and Methods - Exercise/version_two/gym/project/trainer.py
@@ -6,7 +6,7 @@ class Trainer:
 
     @staticmethod
     def get_next_id():
-        return Trainer.id + 1
+        return Trainer.id
 
     def __repr__(self):
         return f"Trainer <{self.id}> {self.name}"
