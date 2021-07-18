cc78de2f89e046ebff6886c3e747ceb8ce31da0a
AntoanStefanov <antoine.stefanov@gmail.com>
2021-07-18 09:37:54 +0300

    customer

--- Advanced/OOP/Attributes and Methods - Exercise/version_two/gym/project/customer.py
+++ Advanced/OOP/Attributes and Methods - Exercise/version_two/gym/project/customer.py
@@ -9,7 +9,7 @@ class Customer:
 
     @staticmethod
     def get_next_id():
-        return Customer.id + 1
+        return Customer.id
 
     def __repr__(self):
         return f"Customer <{self.id}> {self.name}; Address: {self.address}; Email: {self.email}"
