8c78aa2fcfcce58e7f2eb86e3aee06c636a047df
Antoan <antoine.stefanov@gmail.com>
2021-07-29 12:01:10 +0300

    vacationBookList

---
+++ Programming Basics with JavaScript/First Steps In Coding - Exercise/vacationBookList.js
@@ -0,0 +1,10 @@
+function solve(input) {
+    const totalPages = Number(input[0]);
+    const readPagesForHour = Number(input[1]);
+    const availableDays = Number(input[2]);
+    const neededTimeToRead = totalPages / readPagesForHour;
+    const hoursPerDay = neededTimeToRead / availableDays;
+    console.log(hoursPerDay)
+}
+
+solve(['212', '20', '2']);
