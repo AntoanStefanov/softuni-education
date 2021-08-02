aa0fe67478558328dd59fdaeda1d6afc62e71e4d
Antoan <antoine.stefanov@gmail.com>
2021-08-02 21:32:33 +0300

    newHouse

---
+++ Programming Basics with JavaScript/Conditional Statements Advanced - Exercise/newHouse.js
@@ -0,0 +1,21 @@
+function solve(input) {
+    let flowers = input[0];
+    let numberFlowers = Number(input[1]);
+    let budget = Number(input[2]);
+
+    let cost;
+
+    if (flowers === 'Roses') {
+        cost = numberFlowers * 5;
+    } else if (flowers === 'Dahlias') {
+        cost = numberFlowers * 5;
+    } else if (flowers === 'Tulips') {
+        cost = numberFlowers * 2.80;
+    } else if (flowers === 'Narcissus') {
+        cost = numberFlowers * 3;
+    } else if (flowers === 'Gladiolus') {
+        
+    }
+}
+
+solve(['Roses', '55', '250'])
