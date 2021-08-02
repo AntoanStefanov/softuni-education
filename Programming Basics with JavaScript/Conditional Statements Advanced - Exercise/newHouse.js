26c9bac1bf9d94fe1a3348196503cb627f1ac393
Antoan <antoine.stefanov@gmail.com>
2021-08-02 22:13:51 +0300

    newHouse

--- Programming Basics with JavaScript/Conditional Statements Advanced - Exercise/newHouse.js
+++ Programming Basics with JavaScript/Conditional Statements Advanced - Exercise/newHouse.js
@@ -8,7 +8,7 @@ function solve(input) {
     if (flowers === 'Roses') {
         cost = numberFlowers * 5;
     } else if (flowers === 'Dahlias') {
-        cost = numberFlowers * 5;
+        cost = numberFlowers * 3.80;
     } else if (flowers === 'Tulips') {
         cost = numberFlowers * 2.80;
     } else if (flowers === 'Narcissus') {
@@ -16,6 +16,31 @@ function solve(input) {
     } else if (flowers === 'Gladiolus') {
         cost = numberFlowers * 2.50;
     }
+
+    if (flowers === 'Roses' && numberFlowers > 80) {
+        cost *= 0.90;
+    } else if (flowers === 'Dahlias' && numberFlowers > 90) {
+        cost *= 0.85;
+    } else if (flowers === 'Tulips' && numberFlowers > 80) {
+        cost *= 0.85;
+    } else if (flowers === 'Narcissus' && numberFlowers < 120) {
+        cost *= 1.15;
+    } else if (flowers === 'Gladiolus' && numberFlowers < 80) {
+        cost *= 1.20;
+    }
+
+    if (budget >= cost) {
+        console.log(`Hey, you have a great garden with ${numberFlowers} ${flowers} and ${(budget - cost).toFixed(2)} leva left.`)
+    } else {
+        console.log(`Not enough money, you need ${(cost - budget).toFixed(2)} leva more.`)
+    }
 }
 
 solve(['Roses', '55', '250'])
+solve((["Tulips",
+    "88",
+    "260"]))
+
+solve(['Dahlias',
+    '112',
+    '460'])
