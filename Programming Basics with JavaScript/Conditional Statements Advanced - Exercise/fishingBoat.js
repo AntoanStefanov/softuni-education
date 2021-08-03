1ae668cc1323d69f26fec4ec20475627f1b8b89b
Antoan <antoine.stefanov@gmail.com>
2021-08-03 13:51:01 +0300

    fishingBoat

---
+++ Programming Basics with JavaScript/Conditional Statements Advanced - Exercise/fishingBoat.js
@@ -0,0 +1,17 @@
+function solve(input) {
+    let budget = Number(input[0]);
+    let season = input[1];
+    let numberFishers = Number(input[2]);
+
+    let boatCost;
+    
+}
+
+
+solve(["3000",
+    "Summer",
+    "11"])
+
+solve(["3600",
+    "Autumn",
+    "6"])
