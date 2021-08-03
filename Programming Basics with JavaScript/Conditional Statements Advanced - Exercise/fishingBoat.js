f94adebf05d15e77a12e5e0e43b9fbab2e526ad0
Antoan <antoine.stefanov@gmail.com>
2021-08-03 14:41:12 +0300

    fishingBoat

--- Programming Basics with JavaScript/Conditional Statements Advanced - Exercise/fishingBoat.js
+++ Programming Basics with JavaScript/Conditional Statements Advanced - Exercise/fishingBoat.js
@@ -4,7 +4,23 @@ function solve(input) {
     let numberFishers = Number(input[2]);
 
     let boatCost;
-    
+
+    if (season === 'Spring') {
+        boatCost = 3000;
+    } else if (season === 'Summer' || season === 'Autumn') {
+        boatCost = 4200;
+    } else if (season === 'Winter') {
+        boatCost = 2600;
+    }
+
+    if (numberFishers >= 12) {
+        boatCost *= 0.70;
+    } else if (numberFishers >= 7 && numberFishers <= 11) {
+        boatCost *= 0.85;
+    } else {
+        boatCost *= 0.90;
+    }
+
 }
 
 
