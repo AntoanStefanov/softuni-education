02fd1b92d735106f84e3ac4c3667524f30ceb216
AntoanStefanov <antoine.stefanov@gmail.com>
2021-08-05 10:44:36 +0300

    product_repository

---
+++ Advanced/OOP/Inheritance - Exercise/Shop_v_two/project/product_repository.py
@@ -0,0 +1,22 @@
+class ProductRepository:
+    def __init__(self):
+        self.products = []
+
+    def add(self, product):
+        self.products.append(product)
+
+    def find(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                return product
+
+    def remove(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                self.products.remove(product)
+
+    def __repr__(self):
+        res = ''
+        for product in self.products:
+            res += f'{product.name}: {product.quantity}\n'
+        return res[:-1]
