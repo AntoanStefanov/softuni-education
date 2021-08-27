0712204e22d52db508af4496af01ec6de7046116
AntoanStefanov <antoine.stefanov@gmail.com>
2021-08-27 10:40:03 +0300

    add and subtract

---
+++ JS Fundamentals/Functions - Exercise/addAndSubtract.js
@@ -0,0 +1,12 @@
+function solve(a, b, c) {
+    function sum(a, b) {
+        return a + b
+    }
+    function subtract(a, b) {
+        return a - b
+    }
+
+    console.log(subtract(sum(a, b), c));
+}
+
+solve(23, 6, 10)
