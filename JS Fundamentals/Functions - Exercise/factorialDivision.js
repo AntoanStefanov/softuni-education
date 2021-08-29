b85d42e5b8e703f21f5f5318bec3422000f0cfb6
AntoanStefanov <antoine.stefanov@gmail.com>
2021-08-29 13:25:39 +0300

    factorial division

---
+++ JS Fundamentals/Functions - Exercise/factorialDivision.js
@@ -0,0 +1,17 @@
+function solve(numOne, numTwo) {
+    let factorialOne = 1;
+    let factorialTwo = 1;
+
+    for (let i = 1; i <= numOne; i++) {
+        factorialOne *= i;
+    }
+
+    for (let i = 1; i <= numTwo; i++) {
+        factorialTwo *= i;
+    }
+
+    console.log((factorialOne /factorialTwo).toFixed(2));
+}
+
+solve(5, 2);
+solve(6, 2);
