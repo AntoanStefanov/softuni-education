fdd42411b1fa87d91aa6f5c39fe3060f84aa11f1
AntoanStefanov <antoine.stefanov@gmail.com>
2021-09-26 18:33:30 +0300

    app

---
+++ JS Advanced/JS Advanced/DOM Manipulation and Events - Lab/02. Add-Delete/app.js
@@ -0,0 +1,31 @@
+function addItem() {
+    // creating link // 
+    let a = document.createElement('a');
+    let linkText = document.createTextNode('[Delete]');
+    a.appendChild(linkText);
+    a.setAttribute('href', '#');
+    //
+
+    // from 1st task code.
+    let textToAdd = document.getElementById('newItemText').value;
+    let newLiElement = document.createElement('li');
+    newLiElement.appendChild(document.createTextNode(textToAdd));
+    newLiElement.appendChild(a);
+    //
+
+    // a.addEventListener('click', function(e) {
+    //     newLiElement.remove();
+    // })
+
+    let list = document.getElementById('items');
+    list.appendChild(newLiElement);
+
+    list.addEventListener('click', function(event) {
+        if (event.target.tagName === 'A') {
+            let item = event.target.parentElement;
+            item.remove();
+        }
+        
+    });
+
+}
