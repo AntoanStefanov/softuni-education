8b2205dee585656f5b66c06b37ae815352858783
AntoanStefanov <antoine.stefanov@gmail.com>
2021-10-07 14:29:31 +0300

    test

---
+++ JS Advanced/JS Advanced/Unit Testing and Error Handling - Lab/unit-testing-tasks/Check-for-Symmetry/test.js
@@ -0,0 +1,34 @@
+let { expect } = require('chai');
+let { isSymmetric } = require('./checkForSymmetry');
+
+
+describe('05. Check for symmetry', function () {
+    it('should return true, array is symmetric same element-string', function () {
+        expect(isSymmetric(['a', 'a', 'a', 'a'])).to.equal(true);
+    });
+    it('should return true, array is symmetric same element-number', function () {
+        expect(isSymmetric([1, 1, 1, 1])).to.equal(true);
+    });
+    it('should return true, array is symmetric different element', function () {
+        expect(isSymmetric(['a', 'a', 'b', 'a', 'a'])).to.equal(true);
+    });
+    it('should return true, array is symmetric one element', function () {
+        expect(isSymmetric(['a'])).to.equal(true);
+    });
+    it('should return true, array is empty and symmetric', function () {
+        expect(isSymmetric([])).to.equal(true);
+    });
+    // 2 test case judge
+    it('should return false, array is NOT symmetric different type', function () {
+        expect(isSymmetric([1, '1'])).to.equal(false);
+    });
+    it('should return false, incorrect input-string', function () {
+        expect(isSymmetric('incorrect type')).to.equal(false);
+    });
+    it('should return false, incorrect input-number', function () {
+        expect(isSymmetric(1)).to.equal(false);
+    });
+    it('should return false, array is NOT symmetric', function () {
+        expect(isSymmetric(['a', 'a', 'a', 'a', 'b'])).to.equal(false);
+    });
+});
