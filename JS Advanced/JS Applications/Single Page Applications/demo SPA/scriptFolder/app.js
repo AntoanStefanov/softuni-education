2e2d47d2a67b9a38baa9d8fd177de7dc2cc478ad
AntoanStefanov <antoine.stefanov@gmail.com>
2021-11-11 20:09:24 +0200

    app

---
+++ JS Advanced/JS Applications/Single Page Applications/demo SPA/scriptFolder/app.js
@@ -0,0 +1,38 @@
+import { showAboutUsPage } from "./aboutUs.js";
+import { showCatalogPage } from "./catalog.js";
+import { showHomePage } from "./home.js";
+import { showLoginPage } from "./login.js";
+import { onLogout } from "./logout.js";
+import { showRegisterPage } from "./register.js";
+
+document.querySelector('nav').addEventListener('click', navOnClick);
+
+const sections = {
+    'homeBtn': showHomePage,
+    'catalogBtn': showCatalogPage,
+    'aboutUsBtn': showAboutUsPage,
+    'loginBtn': showLoginPage,
+    'registerBtn': showRegisterPage,
+    'logoutBtn': onLogout
+}
+
+// start app in home view
+showHomePage();
+
+function navOnClick(event) {
+    if (event.target.tagName === 'A') {
+
+        const view = sections[event.target.id];
+
+        if (typeof view === 'function') {
+            // chak tuk prevent, za da moje ako e link kum google da go otvori
+            // <!-- <a href="https://www.google.com/?hl=bg">Google</a> -->
+
+            event.preventDefault();
+            view();
+        }
+    }
+}
+
+
+
