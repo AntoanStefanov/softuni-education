c1e581eb70ca54903e5fea62c947774e0e0a4aae
AntoanStefanov <antoine.stefanov@gmail.com>
2022-03-07 11:47:37 +0200

    refactor

--- Perl/Learn Perl 5 By Doing It/join_and_viewing_data_using_dumper/main.pl
+++ Perl/Learn Perl 5 By Doing It/join_and_viewing_data_using_dumper/main.pl
@@ -13,13 +13,15 @@ sub main {
 
     open( my $fh, '<', $input ) or die "\nCannot open $input\n";
 
-    # read first line from file just like that (remove headings)
-    <$fh>;
+    # read first line from file just like that. (remove headings)
+    <$fh>; # Skip first line.
 
     while ( my $line = <$fh> ) {
-        # By default chomp will chomp the $_ variable , $_ is the line if variable $line is not defined.
+
+# By default chomp will chomp the $_ variable , $_ is the line if variable $line is not defined.
         chomp $line;
         my @values = split( /\s*,\s*/, $line );
+
         # print $values[1] . "\n";
 
         # print join(", ", @values);
